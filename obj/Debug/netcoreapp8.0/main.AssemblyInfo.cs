59ec01d78cfd460cc9db56e027cbe770ea777a30
santitrifiro <santiagotrifiro@gmail.com>
2024-10-08 12:30:59 -0300

    Saldo de la tarjeta

--- obj/Debug/netcoreapp8.0/main.AssemblyInfo.cs
+++ obj/Debug/netcoreapp8.0/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9697838cd20c3952d01459319ad7f0df8361a4a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c9e9592b90513438994f293b86bbc2f1f1f13c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
